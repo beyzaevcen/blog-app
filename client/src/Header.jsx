30513345cc2890aad6da1768ae58ecbba965685e
Beyza Evcen <beyzaevcen37@icloud.com>
2025-04-27 14:59:18 +0300

    feat: update header

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -3,26 +3,26 @@ import { useEffect, useState } from "react";
 
 export default function Header() {
   const [userName, setUserName] = useState(null);
-  // useEffect(() => {
-  //   fetch("http://localhost:4000/profile", {
-  //     credentials: "include",
-  //   }).then((response) => {
-  //     response.json().then((userInfo) => {
-  //       setUserName(userInfo.userName);
-  //     });
-  //   });
-  // }, []);
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUserName(userInfo.username);
+      });
+    });
+  }, []);
   return (
     <header>
       <Link to="/" className="logo">
         WW
       </Link>
       <nav>
-        {/* {userName && (
+        {userName && (
           <>
             <Link to="/create">Cretae new post</Link>
           </>
-        )} */}
+        )}
         {!userName && (
           <>
             <Link to="/create">Cretae new post</Link>
